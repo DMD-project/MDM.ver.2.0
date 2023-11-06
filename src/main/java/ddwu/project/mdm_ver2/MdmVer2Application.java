10fcf17645ccf2a4c45049b711acc201a8d7beaf
JURI <jr4426@naver.com>
2023-11-06 22:49:21 +0900

    first commit

---
+++ src/main/java/ddwu/project/mdm_ver2/MdmVer2Application.java
@@ -0,0 +1,13 @@
+package ddwu.project.mdm_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmVer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmVer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ddwu/project/mdm_ver2/MdmVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ddwu.project.mdm_ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmVer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
